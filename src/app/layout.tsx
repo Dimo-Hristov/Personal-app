1c9530dadc8b4c11ae39f1bb94221c6eb479baf3
dimo_xrisrtov@protonmail.com <dimo_xristov@protonmail.com>
2024-02-29 11:21:31 +0200

    create footer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.scss";
 import Navbar from "@/components/navbar/navbar";
+import Footer from "@/components/footer/Footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -20,7 +21,8 @@ export default function RootLayout({
       <body className={inter.className}>
         <Navbar/>
         {children}
-        </body>
+        <Footer/>
+      </body>
     </html>
   );
 }

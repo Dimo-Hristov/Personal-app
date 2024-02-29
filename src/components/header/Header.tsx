65cf153ec343d125d6d79955c4079213aa46d218
dimo_xrisrtov@protonmail.com <dimo_xristov@protonmail.com>
2024-02-29 18:40:59 +0200

    update styles

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -6,7 +6,9 @@ import Image from "next/image"
 const Header = ()=>{
 return(
     <header className={styles.header}>
-        <Image src={'/logo.png'} alt="Logo" width={150} height={100}></Image>
+        <div className={styles.imgContainer}>
+        <Image src={'/logo.png'} alt="Logo" fill/>
+        </div>
     <nav>
         <Links/>
     </nav>

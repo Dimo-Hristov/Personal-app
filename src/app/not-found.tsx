025395e9e2d2d452b707fef886c95c0f17515b5a
dimo_xrisrtov@protonmail.com <dimo_xristov@protonmail.com>
2024-02-29 13:22:04 +0200

    add link to home

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,6 +1,11 @@
+import Link from 'next/link'
+
 const NotFound = ()=>{
     return (
+        <section className="error-page">
         <h2>not found</h2>
+        <Link href={'/'}>Return Home</Link>
+        </section>
     )
 }
 

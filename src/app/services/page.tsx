1eed4ea3c473ebdb02f7e74c66de25a0454abf13
dimo_xrisrtov@protonmail.com <dimo_xristov@protonmail.com>
2024-03-03 23:07:17 +0200

    html & css updates

--- src/app/services/page.tsx
+++ src/app/services/page.tsx
@@ -14,46 +14,61 @@ const services = () => {
         <li>
           <article>
             <div className={styles.titleIconContainer}>
-            <div className={styles.iconContainer}>
-              <MdOutlineScreenshotMonitor />
+              <div className={styles.iconContainer}>
+                <MdOutlineScreenshotMonitor />
               </div>
               <h2>RESPONSIVE APP DEVELOPMENT</h2>
+            </div>
+            <p>
+              Responsive web apps adapt seamlessly to different devices,
+              providing optimal user experiences. They ensure consistent
+              functionality and usability across smartphones, tablets, and
+              desktops.
+            </p>
+          </article>
+        </li>
+        <li>
+          <article>
+            <div className={styles.titleIconContainer}>
+              <div className={styles.iconContainer}>
+                <FaDesktop />
+              </div>
+              <h2>DESKTOP APP DEVELOPMENT</h2>
+            </div>
+            <p>
+              Desktop applications offer powerful functionality tailored for
+              desktop environments, providing robust features and seamless
+              integration with operating systems for enhanced user experiences.
+            </p>
+          </article>
+        </li>
+        <li>
+          <article>
+            <div className={styles.titleIconContainer}>
+              <div className={styles.iconContainer}>
+                <FaMobileAlt />
+              </div>
+              <h2>MOBILE APP DEVELOPMENT</h2>
+            </div>
+            <p>
+              Mobile applications deliver convenience and accessibility,
+              optimized for small screens and touch interactions, ensuring users
+              can access information and services on the go.
+            </p>
+          </article>
+        </li>
+        <li>
+          <article>
+            <div className={styles.titleIconContainer}>
+              <div className={styles.iconContainer}>
+                <MdOutlineDesignServices />
+              </div>
+              <h2>DESIGN, UI AND UX DEVELOPMENT</h2>
+            </div>
 
-            </div>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint cumque in quae impedit cum voluptate quaerat dignissimos, qui culpa voluptatem molestias sapiente ut,.</p>
-          </article>
-        </li>
-        <li>
-          <article>
-          <div className={styles.titleIconContainer}>
-          <div className={styles.iconContainer}>
-              <FaDesktop />
-            </div>
-            <h2>DESKTOP APP DEVELOPMENT</h2>
-          </div>
-           
-          </article>
-        </li>
-        <li>
-          <article>
-          <div className={styles.titleIconContainer}>
-          <div className={styles.iconContainer}>
-              <FaMobileAlt />
-            </div>
-            <h2>MOBILE APP DEVELOPMENT</h2>
-          </div>
-      
-          </article>
-        </li>
-        <li>
-          <article>
-          <div className={styles.titleIconContainer}>
-          <div className={styles.iconContainer}>
-              <MdOutlineDesignServices />
-            </div>
-            <h2>DESIGN, UI AND UX DEVELOPMENT</h2>
-          </div>
-
+            <p>
+            Crafting visually appealing interfaces and seamless user experiences through a blend of creativity and functionality to captivate users and enhance brand engagement.
+            </p>
           </article>
         </li>
       </ul>

4c72a2ef55cfe7ca40ce212b7b835978d00f89ff
dimo_xrisrtov@protonmail.com <dimo_xristov@protonmail.com>
2024-03-03 12:30:06 +0200

    add hamburger menu and implement logic

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,18 +1,31 @@
+'use client'
+
 import Links from "./links/Links"
 import styles from './header.module.scss'
 import Image from "next/image"
 import Link from "next/link"
+import { useEffect, useRef, useState } from "react"
 
 
 const Header = ()=>{
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+
 return(
     <header className={styles.header}>
         <Link href={'/'} className={styles.imgContainer}>
         <Image src={'/logo.png'} alt="Logo" fill/>
         </Link>
-    <nav className={styles.navbar}>
+    <nav className={`${styles.navbar} ${isMenuOpen ? styles.navOpen: styles.navClosed}`}>
         <Links/>
     </nav>
+    <div
+     className={`${styles.hamburger} ${isMenuOpen ? styles.hamOpen: styles.hamClosed}`}
+     onClick={()=> setIsMenuOpen(!isMenuOpen)}
+    >
+      <span></span>
+      <span></span>
+      <span></span>
+    </div>
     </header>
 )
 }

a0296e6b451b98289acf81a924de20cc1c92a40c
dimo_xrisrtov@protonmail.com <dimo_xristov@protonmail.com>
2024-03-03 21:28:22 +0200

    install react-icons, add footer html & css

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,11 +1,31 @@
-import styles from './footer.module.scss'
+import styles from "./footer.module.scss";
+import { FaGithub } from "react-icons/fa";
+import { FaLinkedin } from "react-icons/fa";
+import Link from "next/link";
 
-const Footer = ()=>{
-    return (
-        <footer className={styles.footer}>
-           <h1>footer</h1>
-        </footer>
-    )
-}
+const Footer = () => {
+  return (
+    <footer className={styles.footer}>
+      <div className={styles.wrapper}>
+        <p>&copy; DimoSphere 2024. All rights reserved.</p>
+        <div className={styles.contacts}>
+          <Link
+            href={"https://www.linkedin.com/in/dimo-hristov-626398297/" }
+            target="_blank"
+          >
+            <FaLinkedin />
+          </Link>
 
-export default Footer
+          <Link
+            href={"https://github.com/Dimo-Hristov"}
+            target="_blank"
+          >
+          <FaGithub />
+          </Link>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;

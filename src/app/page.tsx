f54cadc76db951ca2c5369cc0e2559fe5145b023
dimo_xrisrtov@protonmail.com <dimo_xristov@protonmail.com>
2024-03-03 14:28:51 +0200

    Add details to homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,10 @@
-
+import { Pacifico} from "next/font/google";
 import styles from "./page.module.scss";
+const pacifico = Pacifico({
+  subsets: ["latin"],
+  weight: "400"
+})
+
 
 export default function Home() {
 
@@ -8,6 +13,7 @@ export default function Home() {
       <p className={styles.text1}>I&#39;m</p>
         <h1 className={styles.text2}>Dimo Hristov</h1>
         <p className={styles.text3}>Full Stack Developer</p>
+        <p className={`${pacifico.className} ${styles.text4}`}>Code With Passion</p>
     </section>
   );
 }

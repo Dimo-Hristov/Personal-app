af1ad911a497ce2735ae8de57baa8ce5386da228
dimo_xrisrtov@protonmail.com <dimo_xristov@protonmail.com>
2024-03-04 15:12:00 +0200

    complete ContactForm component html & css

--- src/components/contactForm/page.tsx
+++ src/components/contactForm/page.tsx
@@ -1,5 +1,19 @@
+"use client";
+
+import styles from "./contactForm.module.scss";
+
 const ContactForm = () => {
-  return <h1>Contact form</h1>;
+  return (
+    <section className={styles.contactSection}>
+      <form className={styles.contactForm}>
+        <input type="text" name="name" id="name" placeholder="Name" />
+        <input type="text" name="email" id="email" placeholder="Email" />
+        <textarea name="Message" id="message" placeholder="Message" />
+
+        <button> Send</button>
+      </form>
+    </section>
+  );
 };
 
 export default ContactForm;

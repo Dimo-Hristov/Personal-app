fed387aa3d1d4b0ab16dfce128017754aa2c24a9
dimo_xrisrtov@protonmail.com <dimo_xristov@protonmail.com>
2024-03-07 11:58:00 +0200

    add overlay to close the menu on outside click

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -10,29 +10,39 @@ const Header = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
 
   return (
-    <header className={styles.header}>
-      <Link href={"/"} className={styles.imgContainer}>
-        <Image src={"/logo.png"} alt="Logo" fill />
-      </Link>
-      <nav
-        onClick={() => setIsMenuOpen(!isMenuOpen)}
-        className={`${styles.navbar} ${
-          isMenuOpen ? styles.navOpen : styles.navClosed
-        }`}
-      >
-        <Links />
-      </nav>
-      <div
-        className={`${styles.hamburger} ${
-          isMenuOpen ? styles.hamOpen : styles.hamClosed
-        }`}
+    <>
+      {isMenuOpen && (
+        <div
+          className={styles.overlay}
+          onClick={() => setIsMenuOpen(!isMenuOpen)}
+        ></div>
+      )}
+
+      <header
+        className={styles.header}
         onClick={() => setIsMenuOpen(!isMenuOpen)}
       >
-        <span></span>
-        <span></span>
-        <span></span>
-      </div>
-    </header>
+        <Link href={"/"} className={styles.imgContainer}>
+          <Image src={"/logo.png"} alt="Logo" fill />
+        </Link>
+        <nav
+          className={`${styles.navbar} ${
+            isMenuOpen ? styles.navOpen : styles.navClosed
+          }`}
+        >
+          <Links />
+        </nav>
+        <div
+          className={`${styles.hamburger} ${
+            isMenuOpen ? styles.hamOpen : styles.hamClosed
+          }`}
+        >
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
+      </header>
+    </>
   );
 };
 

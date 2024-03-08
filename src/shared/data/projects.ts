0c009e8258f174af2e4e1aeaa46a11560910c208
dimo_xrisrtov@protonmail.com <dimo_xristov@protonmail.com>
2024-03-08 14:12:18 +0200

    add projects descriptions

--- src/shared/data/projects.ts
+++ src/shared/data/projects.ts
@@ -4,31 +4,46 @@ export const projects: Project[] = [
   {
     title: "Racefanatic App",
     description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione ea aliquam alias impedit possimus, minus temporibus explicabo. Est cupiditate qui soluta nihil quasi culpa, eveniet deserunt eos, autem fugit voluptatibus!",
+      "As part of the front-end team, I helped build Bulgaria's first mobile app for motor sports events using Angular and Ionic. My role involved creating a user-friendly interface, implementing features, and ensuring the app worked smoothly on both iOS and Android devices.",
     imageUrl: "/race-app.jpg",
+    type: "Intern",
+    technologies: ["Angular", "ionic", "Typescript"],
+    projectUrl: "",
   },
   {
     title: "Racefanatic Admin Panel",
     description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione ea aliquam alias impedit possimus, minus temporibus explicabo. Est cupiditate qui soluta nihil quasi culpa, eveniet deserunt eos, autem fugit voluptatibus!",
+      "As part of the front-end team for the RaceFanatic app, I contributed to developing the admin panel web application tailored for organizers and administrators. Its a place where organizers can upload  and administrators can manage the events.",
     imageUrl: "/admin-panel.png",
+    type: "",
+    technologies: [],
+    projectUrl: "",
   },
   {
     title: "Apartment Bella",
     description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione ea aliquam alias impedit possimus, minus temporibus explicabo. Est cupiditate qui soluta nihil quasi culpa, eveniet deserunt eos, autem fugit voluptatibus!",
+      "Bella Apartment Website, meticulously crafted to meet the commercial needs of our esteemed client. In alignment with our client's vision, this website is not only SEO-optimized but also boasts exceptional UX/UI design, ensuring a seamless and enjoyable browsing experience.",
     imageUrl: "/bela-apartment.png",
+    type: "",
+    technologies: [],
+    projectUrl: "",
   },
   {
     title: "Auto Explorer",
     description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione ea aliquam alias impedit possimus, minus temporibus explicabo. Est cupiditate qui soluta nihil quasi culpa, eveniet deserunt eos, autem fugit voluptatibus!",
+      "Embark on my journey into the world of React with my inaugural project, Auto Explorer. Crafted with education in mind, this web app serves as a hands-on learning experience.",
     imageUrl: "/auto-explorer.png",
+    type: "",
+    technologies: [],
+    projectUrl: "",
   },
   {
     title: "Travel Forum",
     description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione ea aliquam alias impedit possimus, minus temporibus explicabo. Est cupiditate qui soluta nihil quasi culpa, eveniet deserunt eos, autem fugit voluptatibus!",
+      "Dive into the world of web development with a inaugural project, crafted with Angular. Travel forum serves as a dynamic platform for travel enthusiasts to connect, share experiences, and discover new destinations.",
     imageUrl: "/travel-forum.png",
+    type: "",
+    technologies: [],
+    projectUrl: "",
   },
 ];
--- src/shared/types/Project.ts
+++ src/shared/types/Project.ts
@@ -2,4 +2,7 @@ export type Project = {
   title: string;
   description: string;
   imageUrl: string;
+  type: string;
+  technologies: string[];
+  projectUrl: string;
 };

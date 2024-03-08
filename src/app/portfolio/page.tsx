99d7f4f2635351ad0fa7071b98e7670d3d26420c
dimo_xrisrtov@protonmail.com <dimo_xristov@protonmail.com>
2024-03-08 10:20:44 +0200

    create portfolio structure and styles

--- src/app/portfolio/page.tsx
+++ src/app/portfolio/page.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import styles from "./portfolio.module.scss";
 
 const Portfolio = () => {
@@ -6,7 +7,7 @@ const Portfolio = () => {
       <h1>PORTFOLIO</h1>
 
       <article>
-        <div className="info">
+        <div className={styles.info}>
           <h3>Racefanatic App</h3>
           <p>
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione ea
@@ -14,6 +15,29 @@ const Portfolio = () => {
             cupiditate qui soluta nihil quasi culpa, eveniet deserunt eos, autem
             fugit voluptatibus!
           </p>
+        </div>
+
+        <div className={styles.imageInfo}>
+          <div className={styles.imageContainer}>
+            <Image src={"/race-app.jpg"} alt="race-fantic app" fill></Image>
+          </div>
+        </div>
+      </article>
+      <article>
+        <div className={styles.info}>
+          <h3>Racefanatic Admin Panel</h3>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione ea
+            aliquam alias impedit possimus, minus temporibus explicabo. Est
+            cupiditate qui soluta nihil quasi culpa, eveniet deserunt eos, autem
+            fugit voluptatibus!
+          </p>
+        </div>
+
+        <div className={styles.imageInfo}>
+          <div className={styles.imageContainer}>
+            <Image src={"/admin-panel.png"} alt="race-fantic app" fill></Image>
+          </div>
         </div>
       </article>
     </section>

7f1f30b91d632496e18a4ef44a260edf0b2e7ee2
dimo_xrisrtov@protonmail.com <dimo_xristov@protonmail.com>
2024-03-08 12:15:02 +0200

    complete portfolio page

--- src/app/portfolio/page.tsx
+++ src/app/portfolio/page.tsx
@@ -40,6 +40,65 @@ const Portfolio = () => {
           </div>
         </div>
       </article>
+      <article>
+        <div className={styles.info}>
+          <h3>Apartment Bella</h3>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione ea
+            aliquam alias impedit possimus, minus temporibus explicabo. Est
+            cupiditate qui soluta nihil quasi culpa, eveniet deserunt eos, autem
+            fugit voluptatibus!
+          </p>
+        </div>
+
+        <div className={styles.imageInfo}>
+          <div className={styles.imageContainer}>
+            <Image
+              src={"/bela-apartment.png"}
+              alt="race-fantic app"
+              fill
+            ></Image>
+          </div>
+        </div>
+      </article>
+      <article>
+        <div className={styles.info}>
+          <h3>Travel Forum</h3>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione ea
+            aliquam alias impedit possimus, minus temporibus explicabo. Est
+            cupiditate qui soluta nihil quasi culpa, eveniet deserunt eos, autem
+            fugit voluptatibus!
+          </p>
+        </div>
+
+        <div className={styles.imageInfo}>
+          <div className={styles.imageContainer}>
+            <Image src={"/travel-forum.png"} alt="race-fantic app" fill></Image>
+          </div>
+        </div>
+      </article>
+      <article>
+        <div className={styles.info}>
+          <h3>Auto Explorer</h3>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione ea
+            aliquam alias impedit possimus, minus temporibus explicabo. Est
+            cupiditate qui soluta nihil quasi culpa, eveniet deserunt eos, autem
+            fugit voluptatibus!
+          </p>
+        </div>
+
+        <div className={styles.imageInfo}>
+          <div className={styles.imageContainer}>
+            <Image
+              src={"/auto-explorer.png"}
+              alt="race-fantic app"
+              fill
+            ></Image>
+          </div>
+        </div>
+      </article>
     </section>
   );
 };

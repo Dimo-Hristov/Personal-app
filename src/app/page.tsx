5211e1ccc4afbd93d50d75a53d893ef8e13f018e
dimo_xrisrtov@protonmail.com <dimo_xristov@protonmail.com>
2024-03-09 10:48:31 +0200

    update styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,26 +1,29 @@
-import { Pacifico} from "next/font/google";
+import { Pacifico } from "next/font/google";
 import styles from "./page.module.scss";
 import Link from "next/link";
 
-
 const pacifico = Pacifico({
   subsets: ["latin"],
-  weight: "400"
-})
-
+  weight: "400",
+});
 
 export default function Home() {
-
   return (
     <section className={styles.homepage}>
       <p className={styles.text1}>I&#39;m</p>
-        <h1 className={styles.text2}>Dimo Hristov</h1>
-        <p className={styles.text3}>Full Stack Developer</p>
-        <div className={styles.links}>
-        <Link className={styles.link1} href={'/'}>Download CV</Link>
-        <Link className={styles.link1} href={'/'}>Previous Projects</Link>
-        </div>
-        <p className={`${pacifico.className} ${styles.text4}`}>Code With Passion</p>
+      <h1 className={styles.text2}>Dimo Hristov</h1>
+      <p className={styles.text3}>Full Stack Developer</p>
+      <div className={styles.links}>
+        <Link className={styles.link1} href={"/"}>
+          Download CV
+        </Link>
+        <Link className={styles.link1} href={"/"}>
+          Previous Projects
+        </Link>
+      </div>
+      <p className={`${pacifico.className} ${styles.text4}`}>
+        Code With Passion
+      </p>
     </section>
   );
 }
